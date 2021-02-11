0971d6f6f4172f480e6c631b777013295afbd206
N0 CL1CK <53665362+N0-CL1CK@users.noreply.github.com>
2021-02-11 17:42:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 // Importando módulos
-import { express } from require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-//Importando arquivos locais
-var config = require('./src/configs.json');
 
-//Instanciando a conexão
-var app = express();
+var port = process.env.PORT || 8080;
+
+
+// Arquivos estáticos
+//app.use(express.static('public'))
+//app.use('/css', express.static(__dirname, 'public/css'))
+
+
+// Set Engine Template
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/src/'))
+
+
+//Abrindo a conexão do servidor
+app.listen(port, () => console.log(`[ONLINE] http://localhost:${port}/`));
 
 
 //Instanciando as requisições do servidor
 app.get('/', (_, res) => {
-    res.send('hello world');
-});
-
-
-//Abrindo a conexão do servidor
-app.listen(config.PORT, () => {
-    console.log(`[ONLINE] http://localhost:${config.PORT}`);
+    res.render('pages/home');
 });
